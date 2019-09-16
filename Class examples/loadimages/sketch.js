dea98b934440ad8611663d61b8d2d47b6c3d18d0
Hannna <hmw41@miami.edu>
2019-09-16 16:17:45 -0400

    class

--- Class examples/loadimages/sketch.js
+++ Class examples/loadimages/sketch.js
@@ -8,6 +8,7 @@ function preload(){
 
 function setup() {
   // put setup code here
+  createCanvas(400,400);
 
 }
 
@@ -15,4 +16,9 @@ function draw() {
   // put drawing code here
   // image var, x pos, y pos
 image(puppyImage,0,0);
+//image ver, x, y, width, height
+//dividing width and height by a number scales proportionally
+image(puppyImage,0,0,puppyImage.width/4,puppyImage.height/4);
+//dont choose a random size
+image(puppyImage,200,200, 154,200);
 }
