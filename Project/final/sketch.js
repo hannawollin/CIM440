7b3c70dc9d420cb2602c64774df5542224612c20
Hannna <hmw41@miami.edu>
2019-11-17 15:51:57 -0500

    dfj

--- Project/final/sketch.js
+++ Project/final/sketch.js
@@ -2,8 +2,13 @@ var page=0;  //page 0=home, page 1=tictactoe
 var tictactoe="Tic-Tac-Toe";
 var hometext='Kitty Korner Games';
 var tictactoebutton, homebutton;
+var Oreo, Mika, Stormy;
 
-
+function preload(){
+  Stormy=loadImage('stormysit.png');
+  Mika=loadImage('mikasit.png');
+  Oreo=loadImage('oreosit.png');
+}
 function setup() {
   // put setup code here
   createCanvas(800,500);
@@ -19,8 +24,9 @@ function setup() {
   tictactoebutton.position(50,200);
   tictactoebutton.size(80,80);
   tictactoebutton.style("font-family","Comic Sans MS");
-  tictactoebutton.style("background-color","#00f");
+  tictactoebutton.style("background-color","#32CD32");
   tictactoebutton.style("color","#fff");
+
 }
 
 function draw() {
@@ -30,7 +36,17 @@ function draw() {
   if (page==0){
   background('#75DAFF');
   textSize(50);
-  text(hometext,150,80);
+  fill('black');
+  text(hometext,200,80);
+  textSize(25);
+  fill('blue');
+  image(Oreo, 0,80,Mika.width/2, Mika.height/2 );
+  text("Oreo",240,420);
+  image(Mika, 210,140,Mika.width/2, Mika.height/2);
+  fill('#e75480');
+  text("Mika",460,480);
+  image(Stormy, 390,60,Mika.width/2, Mika.height/2);
+  text("Stormy",625,400);
 }
 
 if (page==1){
